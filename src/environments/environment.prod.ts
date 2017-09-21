aa17beebe17f74e7c82f9f7b7bb92d8ec2e716b7
Mahmoud Ahmed <developer.mahmoudahmed@gmail.com>
2017-09-21 23:41:10 +0200

    Create authetication service and routing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyBw33SqSNY5HgXQOuasm7fpcq0eIi72F9c",
+       authDomain: "oshop-7aed0.firebaseapp.com",
+       databaseURL: "https://oshop-7aed0.firebaseio.com",
+       projectId: "oshop-7aed0",
+       storageBucket: "oshop-7aed0.appspot.com",
+       messagingSenderId: "985673709870"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyBw33SqSNY5HgXQOuasm7fpcq0eIi72F9c",
+       authDomain: "oshop-7aed0.firebaseapp.com",
+       databaseURL: "https://oshop-7aed0.firebaseio.com",
+       projectId: "oshop-7aed0",
+       storageBucket: "oshop-7aed0.appspot.com",
+       messagingSenderId: "985673709870"
+  }
 };
